d54a268ccfb173db2a5361636990653c53bac233
chamin96 <chamin.njay@gmail.com>
2021-04-04 19:36:34 +0530

    mouse events

--- Lesson 2/app.js
+++ Lesson 2/app.js
@@ -4,17 +4,29 @@ const app = Vue.createApp({
       title: "The Last Samurai",
       details: "Captain Nathan Algren is hired to train the Japanese army to fight a Samurai rebellion. However, he ends up embracing the people of Samurai culture after he gets captured by them.",
       rating: 2,
-      showDetails: true
+      showDetails: true,
+      x: 0,
+      y: 0
     };
   },
   methods: {
     handleNext() {
-        this.title = "The Last Airbender",
-        this.details = "The Last Airbender is a 2010 American action-adventure fantasy film written, co-produced and directed by M. Night Shyamalan.",
-        this.rating = 3
+      this.title = "The Last Airbender",
+      this.details = "The Last Airbender is a 2010 American action-adventure fantasy film written, co-produced and directed by M. Night Shyamalan.",
+      this.rating = 3
     },
     handleShowDetails() {
-        this.showDetails = !this.showDetails
+      this.showDetails = !this.showDetails
+    },
+    handleEvent(e, data){
+      console.log(e, e.type);
+      if (data) {
+        console.log(data);
+      }
+    },
+    handleMouseMove(e) {
+      this.x = e.offsetX
+      this.y = e.offsetY
     }
   },
 });
