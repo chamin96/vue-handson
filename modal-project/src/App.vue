6cf8159c55d06fc4b10af22c29ad3c2fd2417801
chamin96 <chamin.njay@gmail.com>
2021-04-05 18:41:32 +0530

    modal project initial

---
+++ modal-project/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>{{ title }} {{ name }}</h1>
+  <input type="text" ref="name" placeholder="your name" />
+  <button @click="handleClick">Okay</button>
+  <Modal :header="header" :subtitle="subtitle" theme="sale"/>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: "App",
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      title: "hello, good afternoon :)",
+      name: "",
+      header: "Welcome to MBox",
+      subtitle: "MBox is the world's largest food community.",
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name.value);
+      this.name = this.$refs.name.value;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="backdrop">
+    <div
+      class="modal"
+      :class="{ sale: theme === 'sale', dark: theme === 'dark' }"
+    >
+      <h1>{{ header }}</h1>
+      <p>{{ subtitle }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["header", "subtitle", "theme"],
+};
+</script>
+
+
+<style scoped>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+h1 {
+  color: seagreen;
+}
+.modal.sale {
+  background: seagreen;
+  color: seashell;
+}
+.modal.sale h1 {
+  color: seashell;
+}
+.modal.dark {
+  background: darkslategray;
+  color: seashell;
+}
+.modal.dark h1 {
+  color: seagreen;
+}
+</style>
