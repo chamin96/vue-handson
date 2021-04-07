f16ebb2f9253fa00097e0e33084b272b00b4b45f
chamin96 <chamin.njay@gmail.com>
2021-04-07 14:17:27 +0530

    init reaction time app

---
+++ reaction-timer/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>HOW FAST ARE YOU?!</h1>
+  <h3>Let's See 😃</h3>
+  <button @click="start" :disabled="isPlaying">PLAY</button>
+  <Block v-if="isPlaying" :delay="delay"/>
+</template>
+
+<script>
+import Block from "./components/Block";
+
+export default {
+  name: "App",
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #e35852;
+  margin-top: 60px;
+}
+</style>
---
+++ reaction-timer/src/components/Block.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">
+    <h3>Click Me</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0,
+    };
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      console.log(this.reactionTime);
+    },
+  },
+  mounted() {
+    console.log("Mounted");
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+    }, this.delay);
+  },
+  //   updated() {
+  //     console.log("Updated");
+  //   },
+  //   unmounted() {
+  //     console.log("Unmounted");
+  //   },
+};
+</script>
+
+<style>
+.block {
+  width: 300px;
+  border-radius: 20px;
+  background: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 50px 0;
+  margin: 40px auto;
+}
+</style>
