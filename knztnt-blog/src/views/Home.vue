0e06671b15ea4540cd0fa66836d91f3151bf8cd1
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2021-04-19 11:07:34 +0530

    init

---
+++ knztnt-blog/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <p>My name is {{ name }} and I am {{ age }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  setup() {
+    let name = "Chamin";
+    let age = 25;
+
+    return { name, age };
+  },
+};
+</script>
